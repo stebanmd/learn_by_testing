107386110fafae37e2cefbc5048c503398c6057f
Steban Domingues <steban.md@gmail.com>
2019-07-17 17:11:27 -0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyA7X8UKQZGFlKmGKhSCdWUlGKFhGTyiBjc',
-    authDomain: 'jav-flashcards.firebaseapp.com',
-    databaseURL: 'https://jav-flashcards.firebaseio.com',
-    projectId: 'jav-flashcards',
-    storageBucket: '',
-    messagingSenderId: '104648796114',
-    appId: '1:104648796114:web:476a614a5f0be026'
-  }
+  firebase: {}
 };
 
 /*
